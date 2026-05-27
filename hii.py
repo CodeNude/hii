a483c63296a9221906fbfa172c0fc1ce6239bd4e
codeNude <redmibyxiamoi@gmail.com>
2026-05-27 11:47:40 +0530

    hii wowo

--- hii.py
+++ hii.py
@@ -2,8 +2,5 @@ class hello_world:
     def __init__(self):
         pass
 
-    def say_hello(self):
-        return "Hello, World!"
-    
-    def saygoodbye(self):
-        return "Goodbye, World!"
+    def saybye(self):
+        return "hii wowo!"
